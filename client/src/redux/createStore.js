3d31594d7c93e5d7e1b62793613bd4771b2e4036
EclipticWld <ecliptic.wld@gmail.com>
2016-11-17 23:49:43 +0300

    add redux-saga example

--- client/src/redux/createStore.js
+++ client/src/redux/createStore.js
@@ -2,9 +2,12 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import createLogger from 'redux-logger'
 import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
+import { helloSaga } from '../sagas'
 
 export default (initialState = {}) => {
-  const middlewares = [thunk]
+  const sagaMiddleware = createSagaMiddleware()
+  const middlewares = [thunk, sagaMiddleware]
   if (process.env.NODE_ENV !== 'production') {
     middlewares.push(createLogger())
   }
@@ -13,6 +16,8 @@ export default (initialState = {}) => {
     rootReducer,
     applyMiddleware(...middlewares)
   )
+
+  sagaMiddleware.run(helloSaga)
 
   return store
 }
